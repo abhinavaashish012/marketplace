cfbdc9ba503ef8c666c1859a6123ec979dc265cb
Abhinav <abhinavaashish012@gmail.com>
2020-11-27 19:04:07 +0530

    some-changes

--- estore/users/urls.py
+++ estore/users/urls.py
@@ -11,4 +11,5 @@ urlpatterns=[
     url('get-equip/',user_view.get_equip,name='get_equip'),
     url('get_stationery/',user_view.get_stationery,name='get_stationery'),
     url('login_page/',user_view.login_page,name='login_page'),
+    url("register/", user_view.register, name="register"),
 ]
--- estore/users/views.py
+++ estore/users/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, authenticate
+from .forms import SignUpForm
+import json,xmltodict
 # Create your views here.
 app_name='users'
 def welcome_page(request):
@@ -25,5 +28,26 @@ def get_stationery(request):
     return render(request,"users/stationery.html")
 
 def login_page(request):
-    print(request.POST)
-    return render(request,"users/login_page.html")
+    print(request)
+    return render(request,"users/login_page.html")#,detail)
+
+def register(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            data_dict=dict()
+            print("Form Submitted")
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            data_dict = xmltodict.parse(form)
+            print(json.dumps(data_dict))
+            print("------------------------------")
+            print(form)
+            login(request, user)
+            return redirect('users/homepage.html')
+    else:
+        form = SignUpForm()
+    return render(request, 'users/user_reg.html', {'form': form})
+
